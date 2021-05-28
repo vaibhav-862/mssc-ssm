0381109ef532bfa9e4cf4939ec51aabac5aeb0fe
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-28 13:48:26 +0530

    adding payment service

---
+++ src/main/java/guru/springframework/msscssm/service/PaymentService.java
@@ -0,0 +1,19 @@
+package guru.springframework.msscssm.service;
+
+import org.springframework.statemachine.StateMachine;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+
+public interface PaymentService {
+	
+	Payment newPayment(Payment payment);
+	
+	StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId);
+	
+	StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId);
+	
+	StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId);
+	
+}
---
+++ src/main/java/guru/springframework/msscssm/service/PaymentServiceImpl.java
@@ -0,0 +1,41 @@
+package guru.springframework.msscssm.service;
+
+import org.springframework.statemachine.StateMachine;
+import org.springframework.statemachine.config.StateMachineFactory;
+import org.springframework.stereotype.Service;
+
+import guru.springframework.msscssm.domain.Payment;
+import guru.springframework.msscssm.domain.PaymentEvent;
+import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.repository.PaymentRepository;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class PaymentServiceImpl implements PaymentService {
+
+	private final PaymentRepository paymentRepository;
+	private final StateMachineFactory<PaymentState, PaymentEvent> stateMachineFactory;
+	
+	@Override
+	public Payment newPayment(Payment payment) {
+		payment.setState(PaymentState.NEW);
+		return paymentRepository.save(payment);
+	}
+
+	@Override
+	public StateMachine<PaymentState, PaymentEvent> preAuth(Long paymentId) {
+		return null;
+	}
+
+	@Override
+	public StateMachine<PaymentState, PaymentEvent> authorizePayment(Long paymentId) {
+		return null;
+	}
+
+	@Override
+	public StateMachine<PaymentState, PaymentEvent> declineAuth(Long paymentId) {
+		return null;
+	}
+
+}
