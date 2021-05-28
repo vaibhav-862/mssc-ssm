0d60e0cbf004201e639ff38c31d114da7d369c26
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-28 12:52:24 +0530

    adding state transitions

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineStateConfigurer;
+import org.springframework.statemachine.config.builders.StateMachineTransitionConfigurer;
 
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
@@ -14,7 +15,6 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @EnableStateMachineFactory //specifies to spring about building state machine factory
 @Configuration
-//public class StateMachineConfig {
 public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentState, PaymentEvent>{
 
 	@Override
@@ -28,4 +28,14 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 				.end(PaymentState.AUTH_ERROR); //failure termination state
 		}
 	
+	@Override
+	public void configure(StateMachineTransitionConfigurer<PaymentState, PaymentEvent> transitions) throws Exception {
+		// TODO Auto-generated method stub
+		//state transition from source to target upon receiving the specified event
+		transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+			.and()
+			.withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
+			.and()
+			.withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH_ERROR).event(PaymentEvent.PRE_AUTH_DECLINED);
+	}
 }
