b7f135ddeba85f1c8098d4e7367c3c0b94641f28
Vaibhav Dubey <vaibhav.dubey@hpe.com>
2021-05-28 17:07:27 +0530

    adding state machine actions

--- src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
+++ src/main/java/guru/springframework/msscssm/config/StateMachineConfig.java
@@ -1,8 +1,11 @@
 package guru.springframework.msscssm.config;
 
 import java.util.EnumSet;
+import java.util.Random;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.support.MessageBuilder;
+import org.springframework.statemachine.action.Action;
 import org.springframework.statemachine.config.EnableStateMachineFactory;
 import org.springframework.statemachine.config.StateMachineConfigurerAdapter;
 import org.springframework.statemachine.config.builders.StateMachineConfigurationConfigurer;
@@ -11,8 +14,10 @@ import org.springframework.statemachine.config.builders.StateMachineTransitionCo
 import org.springframework.statemachine.listener.StateMachineListenerAdapter;
 import org.springframework.statemachine.state.State;
 
+import ch.qos.logback.core.Context;
 import guru.springframework.msscssm.domain.PaymentEvent;
 import guru.springframework.msscssm.domain.PaymentState;
+import guru.springframework.msscssm.services.PaymentServiceImpl;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -36,6 +41,7 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 		// TODO Auto-generated method stub
 		//state transition from source to target upon receiving the specified event
 		transitions.withExternal().source(PaymentState.NEW).target(PaymentState.NEW).event(PaymentEvent.PRE_AUTHORIZE)
+					.action(preAuthAction())
 			.and()
 			.withExternal().source(PaymentState.NEW).target(PaymentState.PRE_AUTH).event(PaymentEvent.PRE_AUTH_APPROVED)
 			.and()
@@ -55,5 +61,27 @@ public class StateMachineConfig extends StateMachineConfigurerAdapter<PaymentSta
 		
 		config.withConfiguration()
 				.listener(adapter);
+	}
+	
+
+	private Action<PaymentState, PaymentEvent> preAuthAction() {
+		// TODO Auto-generated method stub
+		return context -> {
+			System.out.println("PreAuth was called!!");
+			
+			if(new Random().nextInt(10) < 8) {
+				System.out.println("Approved");
+				
+				context.getStateMachine().sendEvent(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_APPROVED)
+						.setHeader(PaymentServiceImpl.PAYMENT_ID_HEADER, context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER))
+						.build());
+			} else {
+				System.out.println("Declined!! No Credit!!!");
+				
+				context.getStateMachine().sendEvent(MessageBuilder.withPayload(PaymentEvent.PRE_AUTH_DECLINED)
+						.setHeader(PaymentServiceImpl.PAYMENT_ID_HEADER, context.getMessageHeader(PaymentServiceImpl.PAYMENT_ID_HEADER))
+						.build());
+			}
+		};
 	}
 }
--- src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
+++ src/test/java/guru/springframework/msscssm/services/PaymentServiceImplTest.java
@@ -36,12 +36,14 @@ public class PaymentServiceImplTest {
 	void preAuth() {
 		Payment savedPayment = paymentService.newPayment(payment);
 		
+		System.out.println("Should be NEW");
 		System.out.println(savedPayment.getState());
 		
 		StateMachine<PaymentState, PaymentEvent> sm = paymentService.preAuth(savedPayment.getId());
 		
 		Payment preAuthedPayment = paymentRepository.getOne(savedPayment.getId());
 		
+		System.out.println("Should be PRE_AUTH or PRE_AUTH_ERROR");
 		System.out.println(sm.getState().getId());
 		
 		System.out.println(preAuthedPayment);
